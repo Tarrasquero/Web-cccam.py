b3799820720e928d9f7fe68c7e61367e203578cc
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-06-12 22:12:04 +0200

    Add files via upload

---
+++ Web_Oscam.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from requests.auth import HTTPDigestAuth
+import requests
+import os
+from commands import getoutput
+cccam = os.path.abspath(os.path.join(__file__, os.pardir, 'CCcam.cfg'))
+cccam = open("CCcam.cfg", "r")
+linea = cccam.readline()
+server = getoutput('cat CCcam.cfg|grep "C:" | shuf -n1')
+device = server.split(' ')
+port = device[2]
+user = device[3]
+password = device[4]
+device = device[1]
+params = {"id": "server.config", "action": "execute",
+          "label": "Reader-7", "protocol": "cccam",
+          "device": device, "port": port,
+          "user": user, "password": password, "action": "Save"
+          }
+url = 'http://192.168.1.56:8080/server_config.html?label=Reader-7'
+r = requests.get(url, auth=HTTPDigestAuth('jorge', '0000'), params=params)
+
+print(r.status_code)
